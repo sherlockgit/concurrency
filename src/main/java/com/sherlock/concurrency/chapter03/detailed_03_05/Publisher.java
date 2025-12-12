721abc862027de05fe52e7785a673c0c7c9da5aa
sherlock <1243935366@qq.com>
2025-12-12 16:09:59 +0800

    发布一个对象

--- src/main/java/com/sherlock/concurrency/chapter03/detailed_03_05/Publisher.java
+++ src/main/java/com/sherlock/concurrency/chapter03/detailed_03_05/Publisher.java
@@ -7,7 +7,7 @@ import java.util.List;
  * 发布一个对象
  *
  * 发布对象的最简单方法是将对象的引用保存到一个公有的静态变量中，
- * 以便任何类和线程都能看见该对象，在initialize方法中实例化一个新的 HashSet对象，
+ * 以便任何类和线程都能看见该对象，在initialize方法中实例化一个新的 List对象，
  * 并将对象的引用保存到 knownSecrets中以发布该对象。
  */
 public class Publisher {
