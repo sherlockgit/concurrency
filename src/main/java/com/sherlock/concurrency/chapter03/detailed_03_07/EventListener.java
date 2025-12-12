fd9396e1e84d5729c44ebc40f2a68036fdd31415
sherlock <1243935366@qq.com>
2025-12-12 17:58:55 +0800

    使内部可变状态逸出

---
+++ src/main/java/com/sherlock/concurrency/chapter03/detailed_03_07/EventListener.java
@@ -0,0 +1,9 @@
+package com.sherlock.concurrency.chapter03.detailed_03_07;
+
+import com.sun.jdi.event.Event;
+
+public interface EventListener {
+
+    void onEvent(Event event);
+
+}
---
+++ src/main/java/com/sherlock/concurrency/chapter03/detailed_03_07/EventSource.java
@@ -0,0 +1,42 @@
+package com.sherlock.concurrency.chapter03.detailed_03_07;
+
+import com.sun.jdi.event.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: linmuyu
+ * @Date: 2025/12/12 16:22
+ */
+public class EventSource {
+
+    // 存储监听器列表
+    private List<EventListener> listeners = new ArrayList<>();
+
+    // 注册监听器
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
+    }
+
+    // 移除监听器
+    public void unregisterListener(EventListener listener) {
+        listeners.remove(listener);
+    }
+
+    // 触发事件，通知所有监听器
+    public void fireEvent(Event event) {
+        for (EventListener listener : listeners) {
+            listener.onEvent(event);
+        }
+    }
+
+    // 可能还有异步触发事件的方法
+    public void fireEventAsync(Event event) {
+        new Thread(() -> {
+            fireEvent(event);
+        }).start();
+    }
+
+}
