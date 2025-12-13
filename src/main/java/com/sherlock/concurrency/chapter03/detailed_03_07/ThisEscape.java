c56d3935bbef401044034da54e28329c0283b388
sherlock <1243935366@qq.com>
2025-12-13 16:40:27 +0800

    基本类型的局部变量与引用变量的线程封闭性

--- src/main/java/com/sherlock/concurrency/chapter03/detailed_03_07/ThisEscape.java
+++ src/main/java/com/sherlock/concurrency/chapter03/detailed_03_07/ThisEscape.java
@@ -4,7 +4,7 @@ import com.sherlock.concurrency.annoations.NotRecommend;
 import com.sun.jdi.event.Event;
 
 /**
- * 使内部可变状态逸出（不要这么做）
+ *  （不要这么做）
  *
  *  如果按照上述方式来发布states，就会出现问题，因为任何调用者都能修改这个数组的内容。
  *  在这个示例中，数组 states已经逸出了它所在的作用域，因为这个本应是私有的变量已经被发布了。
